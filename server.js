dfce50398426d6e327d1e88a461eb327df6eeb4f
MAAhi-Bloom <czone3144@gmail.com>
2024-11-15 20:23:56 +0600

    changes something in server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = 3008;
 
 app.use("/bloom/", express.static(path.join(__dirname, "public/bloom")));
 app.use("/imagesho/", express.static(path.join(__dirname, "public/imagesho")));
